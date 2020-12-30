2488932c2fe1916f05bf448333a8296da7c352d3
mackliu <macklun@gmail.com>
2020-12-30 09:49:55 +0800

    完成切版及include程式

--- backend.php
+++ backend.php
@@ -21,6 +21,19 @@
     </marquee>
   </div>
   <div id="mm">
+  <?php
+  //$do=isset($_GET['do'])?$_GET['do']:'main';
+  $do=isset($_GET['do'])??'main';
+  $file='back/'.$do.".php";
+  if(file_exists($file)){
+    include $file;
+  }else{
+    include "back/main.php";
+  }
+
+  //include (file_exists($file))?$file:"front/main.php";
+
+  ?>  
     <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;"> <a href="?do=admin&redo=tit">網站標題管理</a>| <a href="?do=admin&redo=go">動態文字管理</a>| <a href="?do=admin&redo=rr">預告片海報管理</a>| <a href="?do=admin&redo=vv">院線片管理</a>| <a href="?do=admin&redo=order">電影訂票管理</a> </div>
     <div class="rb tab">
       <h2 class="ct">請選擇所需功能</h2>

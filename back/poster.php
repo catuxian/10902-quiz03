44b2596c32cf8931825cb9437a98dbe3a661248e
mackliu <macklun@gmail.com>
2020-12-30 16:08:19 +0800

    完成預告片後台各項功能

--- back/poster.php
+++ back/poster.php
@@ -6,21 +6,35 @@
         <div style="width:25%;margin:0 1px;background:#999">預告片排序</div>
         <div style="width:25%;margin:0 1px;background:#999">操作</div>
     </div>
+    <form action="api/edit_poster.php" method='post'>
     <div style="width:100%;height:200px;overflow-y:scroll;color:black">
     <?php
-    $posters=$Poster->all();
-    foreach($posters as $poster){
+    $posters=$Poster->all(" order by rank");
+    foreach($posters as $key => $poster){
     ?>
     <div style="width:100% ;display:flex ;align-items:center;background:white;margin:1px 0" class='ct'>
         <div style="width:25%;margin:0 1px">
             <img src="img/<?=$poster['img'];?>" style="width:80px">
         </div>
         <div style="width:25%;margin:0 1px">
-            <input type="text" name="name" value="<?=$poster['name'];?>">
+            <input type="text" name="name[]" value="<?=$poster['name'];?>">
         </div>
         <div style="width:25%;margin:0 1px">
-            <input type="button" value="往上">
-            <input type="button" value="往下">
+            <?php
+                if($key!=0){
+            ?>
+                <input type="button" value="往上" onclick="sw(<?=$poster['id'];?>,<?=$posters[$key-1]['id'];?>)">
+            <?php     
+                }
+            ?>
+
+            <?php
+                if($key!=(count($posters)-1)){
+            ?>
+                <input type="button" value="往下" onclick="sw(<?=$poster['id'];?>,<?=$posters[$key+1]['id'];?>)">
+            <?php
+            }
+            ?>
         </div>
         <div style="width:25%;margin:0 1px">
             <input type="checkbox" name="sh[]" value="<?=$poster['id'];?>" <?=($poster['sh']==1)?'checked':'';?>>顯示
@@ -31,6 +45,7 @@
                 <option value="3" <?=($poster['ani']==3)?"selected":'';?>>縮放</option>
             
             </select>
+            <input type="hidden" name="id[]" value="<?=$poster['id'];?>">
         </div>
     </div>
     <?php    
@@ -41,6 +56,8 @@
     <div class="ct">
         <input type="submit" value="編輯確定"><input type="reset" value="重置">
     </div>
+    </form>
+
     <hr>
     <h3 class="ct" style="margin:0">新增預告片海報</h3>
     <form action="api/add_poster.php" method="post" enctype="multipart/form-data">
@@ -59,3 +76,11 @@
     </div>
     </form>
 </div>
+<script>
+function sw(idx,idy){
+    $.post('api/sw.php',{table:'poster',idx,idy},function(){
+        location.reload()
+    })
+}
+
+</script>

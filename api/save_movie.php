11ccc6e6b9818f30cc41b9e769aa1ef1e161c0ed
mackliu <macklun@gmail.com>
2020-12-31 11:39:33 +0800

    合併新增/編輯電影為一支API

---
+++ api/save_movie.php
@@ -0,0 +1,29 @@
+<?php
+include_once "../base.php";
+
+if(!empty($_FILES['trailer']['tmp_name'])){
+    $_POST['trailer']=$_FILES['trailer']['name'];
+    move_uploaded_file($_FILES['trailer']['tmp_name'],'../img/'.$_FILES['trailer']['name']);
+}
+if(!empty($_FILES['poster']['tmp_name'])){
+    $_POST['poster']=$_FILES['poster']['name'];
+    move_uploaded_file($_FILES['poster']['tmp_name'],'../img/'.$_FILES['poster']['name']);
+}
+
+if(isset($_POST['id'])){
+    //編輯
+    $movie=$Movie->find($_POST['id']);
+    $_POST['sh']=$movie['sh'];
+    $_POST['rank']=$movie['rank'];
+    
+}else{
+    //新增
+    $_POST['sh']=1;
+    $_POST['rank']=$Movie->q("select max(rank) from movie")[0][0]+1;
+}
+
+$Movie->save($_POST);
+
+to("../backend.php?do=movie");
+
+?>

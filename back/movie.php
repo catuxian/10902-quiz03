39ca8b3dcd0cb226b6dd0d2e7d5da6e8fb2cc78e
mackliu <macklun@gmail.com>
2020-12-31 13:39:41 +0800

    完成排序及顯示切換功能

--- back/movie.php
+++ back/movie.php
@@ -4,8 +4,8 @@
 <div style="max-height:450px;overflow-y:auto">
 
 <?php
-    $movies=$Movie->all();
-    foreach($movies as $movie){
+    $movies=$Movie->all(" order by rank");
+    foreach($movies as $key => $movie){
 ?>
 <div style="background:white;color:black;display:flex;margin:1px 0">
     <div style="width:20%">
@@ -21,8 +21,24 @@
             <div style="width:33%">上映時間:<?=$movie['year'];?>-<?=$movie['month'];?>-<?=$movie['day'];?></div>
         </div>
         <div style="text-align:right">
+        <button onclick="display('movie',<?=$movie['id'];?>)"><?=($movie['sh']==1)?'顯示':'隱藏';?></button>
+        <?php
+                if($key!=0){
+            ?>
+                <button onclick="sw(<?=$movie['id'];?>,<?=$movies[$key-1]['id'];?>)">往上</button>
+            <?php     
+                }
+            ?>
+
+            <?php
+                if($key!=(count($movies)-1)){
+            ?>
+                <button onclick="sw(<?=$movie['id'];?>,<?=$movies[$key+1]['id'];?>)">往下</button>
+            <?php
+            }
+            ?>
             <button onclick="javascript:location.href='backend.php?do=edit_movie&id=<?=$movie['id'];?>'">編輯電影</button>
-            <button>刪除電影</button>
+            <button onclick="del('movie',<?=$movie['id'];?>)">刪除電影</button>
 
         </div>
         <div><?=$movie['intro'];?></div>
@@ -31,16 +47,26 @@
 <?php
 }
 ?>
-
-
 </div>
 
 </div>  
 <script>
 function sw(idx,idy){
-    $.post('api/sw.php',{table:'poster',idx,idy},function(){
+    $.post('api/sw.php',{table:'movie',idx,idy},function(){
+        
         location.reload()
     })
 }
 
+function del(table,id){
+    $.post('api/del.php',{table,id},function(){
+        location.reload()
+    })
+}
+
+function display(table,id){
+    $.post('api/display.php',{table,id},function(){
+        location.reload()
+    })
+}
 </script>

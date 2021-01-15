6dc6cf022128036a73adf613eb7eb0ed26445724
mackliu <macklun@gmail.com>
2021-01-15 11:42:20 +0800

    完成快速刪除訂單功能

--- back/order.php
+++ back/order.php
@@ -22,6 +22,23 @@
 
 <div class="rb tab" style="width:98%">
     <div class="ct">訂單清單</div>
+    <div class="q-del">
+        快速刪除:
+        <input type="radio" name="type" value="date" checked>依日期
+        <input type="date" name="date" id='date'>
+        <input type="radio" name="type" value="movie">依電影
+        <select name="movie" id="movie">
+        <?php
+        $movies=$Movie->q("select `movie` from `orders` group by `movie`");
+        foreach($movies as $movie){
+            echo "<option value='{$movie['movie']}'>{$movie['movie']}</option>";
+        }
+        ?>
+        </select>
+        <button onclick="qDel()">刪除</button>
+
+
+    </div>
     <div class="ord-header">
         <div>訂單編號</div>
         <div>電影名稱</div>
@@ -42,7 +59,14 @@
             <div><?=$ord['date'];?></div>
             <div><?=$ord['session'];?></div>
             <div><?=$ord['qt'];?></div>
-            <div><?=$ord['seats'];?></div>
+            <div><?php
+            
+            $seats=unserialize($ord['seats']);
+            foreach($seats as $seat){
+                echo (floor($seat/5)+1)."排".($seat%5+1)."號<br>";
+            }
+
+            ?></div>
             <div><button onclick="del('orders',<?=$ord['id'];?>)">刪除</button></div>
         </div>
         <hr>
@@ -57,5 +81,28 @@ function del(table,id){
     $.post('api/del.php',{table,id},function(){
         location.reload()
     })
+}
+
+function qDel(){
+    let value
+    let type=$("input[type='radio']:checked").val();
+    switch(type){
+        case "date":
+            value=$("#date").val()
+        break;
+        case "movie":    
+            value=$("#movie").val()
+        break;
+    }
+
+    let con=confirm('你確定要刪除所有'+value+'的訂單嗎?')
+    
+    if(con){
+        $.post('api/qdel.php',{value,type},function(){
+            location.reload()
+        })
+    }
+
+
 }
 </script>
